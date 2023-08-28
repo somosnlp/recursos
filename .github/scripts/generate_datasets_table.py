53072ff5f2cff06de4f41aba1a742feb51b45add
Maria Grandury <mariagrandury@gmail.com>
2023-08-28 17:18:09 +0200

    chore: use delimiters to generate readme

--- .github/scripts/generate_datasets_table.py
+++ .github/scripts/generate_datasets_table.py
@@ -10,9 +10,19 @@ df.to_csv("datasets.csv", index=False)
 table_content = df.to_markdown(index=False)
 
 with open("README.md", "r") as f:
-    readme_content = f.read()
+    readme_template = f.read()
 
-updated_readme_content = readme_content.replace("<!-- TABLE_CONTENT -->", table_content)
+start_delimiter = "<!-- START_TABLE_CONTENT -->"
+end_delimiter = "<!-- END_TABLE_CONTENT -->"
+
+start_pos = readme_template.find(start_delimiter)
+end_pos = readme_template.find(end_delimiter)
+
+updated_readme_content = (
+    readme_template[:start_pos + len(start_delimiter)]
+    + "\n" + table_content + "\n"
+    + readme_template[end_pos:]
+)
 
 with open("README.md", "w") as f:
     f.write(updated_readme_content)
